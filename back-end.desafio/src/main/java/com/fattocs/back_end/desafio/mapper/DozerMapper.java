549cc94208f7d07191d3306adeb262fc1eb9886c
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-10-30 10:00:41 -0300

    Dozer Mapper

---
+++ back-end.desafio/src/main/java/com/fattocs/back_end/desafio/mapper/DozerMapper.java
@@ -0,0 +1,26 @@
+package com.fattocs.back_end.desafio.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+//Poderia usar o model mapper, mas me foi aconselhado a preferir o Dozer por pessoas com mais experiência.
+public class DozerMapper {
+
+    private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+        List<D> destinationObjects = new ArrayList<D>();
+        for (O o : origin) {
+            destinationObjects.add(mapper.map(o, destination));
+        }
+        return destinationObjects;
+    }
+
+}
