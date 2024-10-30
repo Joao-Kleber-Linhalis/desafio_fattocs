1678b63db3392d4d730c7e14c2901c5e31b5cabf
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-10-30 09:17:43 -0300

    first commit

---
+++ back-end.desafio/src/main/java/com/fattocs/back_end/desafio/BackEndDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.fattocs.back_end.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndDesafioApplication.class, args);
+	}
+
+}
---
+++ back-end.desafio/src/test/java/com/fattocs/back_end/desafio/BackEndDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fattocs.back_end.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
