56c7426a18b9ad3eb98cc5acadb82af27fc31b74
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-11-05 16:59:35 -0300

    basic structure

---
+++ front-end.desafio/src/app/services/task.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private http: HttpClient) { }
+
+  findById(id: any): Observable<Task> {
+    return this.http.get<Task>(`${API_CONFIG.baseUrl}/task/v1/${id}`)
+  }
+
+  findAll(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${API_CONFIG.baseUrl}/task/v1`);
+  }
+
+  create(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${API_CONFIG.baseUrl}/task/v1`, task);
+  }
+
+  update(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${API_CONFIG.baseUrl}/task/v1/${task.id}`, task);
+  }
+
+  delete(id: any): Observable<Task> {
+    return this.http.delete<Task>(`${API_CONFIG.baseUrl}/task/v1/${id}`)
+  }
+
+  updatePresentationOrder(id: any, newOrder: number): Observable<Task> {
+    return this.http.patch<Task>(`${API_CONFIG.baseUrl}/task/v1/${id}/${newOrder}`, null);
+  }
+}
