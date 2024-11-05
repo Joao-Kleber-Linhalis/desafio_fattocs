fed8e64794aa4bbc0351b3582b2a0fec6b9bf63d
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-11-05 16:58:23 -0300

    remove line

--- back-end.desafio/src/main/java/com/fattocs/back_end/desafio/controllers/TaskController.java
+++ back-end.desafio/src/main/java/com/fattocs/back_end/desafio/controllers/TaskController.java
@@ -117,7 +117,6 @@ public class TaskController {
                     ),
                     @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
                     @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
                     @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
             }
