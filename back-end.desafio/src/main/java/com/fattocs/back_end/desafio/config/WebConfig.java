2dfc79929e3809217cd935d973a09bd6bb99387d
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-11-05 18:36:11 -0300

    fix cors

--- back-end.desafio/src/main/java/com/fattocs/back_end/desafio/config/WebConfig.java
+++ back-end.desafio/src/main/java/com/fattocs/back_end/desafio/config/WebConfig.java
@@ -5,16 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 
-    @Value("${cors.originPatterns:default")
+    @Value("${cors.originPatterns}")
     private String corsOriginPatterns = "";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         var allowedOrigins = corsOriginPatterns.split(",");
+        System.out.println("Allowed Origins: " + Arrays.toString(allowedOrigins));  // Para ver as origens no console
+
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins(allowedOrigins);
